a68dfa45649022649b4461fc0b1806433edb1588
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2020-12-19 23:30:43 +0900

    Update example program

--- RDKit2DotNet.Example/RDKit2DotNet.Example/Program.cs
+++ RDKit2DotNet.Example/RDKit2DotNet.Example/Program.cs
@@ -3,17 +3,10 @@ using System;
 using System.IO;
 using System.Text;
 
-namespace RDKit2DotNet.Example
+namespace RDKitCSharpTest
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            ErrorHandle();
-            Demo();
-            Test();
-        }
-
         static void ErrorHandle()
         {
             var invalid = RWMol.MolFromSmiles("dfep3js2g");
@@ -21,12 +14,12 @@ namespace RDKit2DotNet.Example
 
         static void Demo()
         { 
-            var toluene = RWMol.MolFromSmiles("Cc1ccccc1");
-            var mol1 = RWMol.MolFromMolFile("Data/input.mol");
-            var stringWithMolData = new StreamReader("Data/input.mol").ReadToEnd();
+            var toluene = RWMol.MolFromSmiles("Cc1ccccc1");            
+            var mol1 = RWMol.MolFromMolFile(Path.Combine("Data", "input.mol"));
+            var stringWithMolData = new StreamReader(Path.Combine("Data", "input.mol")).ReadToEnd();
             var mol2 = RWMol.MolFromMolBlock(stringWithMolData);
 
-            using (var suppl = new SDMolSupplier("Data/5ht3ligs.sdf"))
+            using (var suppl = new SDMolSupplier(Path.Combine("Data", "5ht3ligs.sdf")))
             {
                 while (!suppl.atEnd())
                 {
@@ -67,10 +60,9 @@ namespace RDKit2DotNet.Example
             return sb.ToString();
         }
 
-        static void Test()
+        static void CreateSomeObjects()
         {
             // ----- Object creation -----
-
             Console.WriteLine("Creating some objects:");
 
             ROMol m1 = RWMol.MolFromSmiles("c1ccccc1");
@@ -84,23 +76,62 @@ namespace RDKit2DotNet.Example
 
             Console.WriteLine(" sim: " + RDKFuncs.TanimotoSimilarityEBV(fp1, fp2));
 
-            //rxnTest();
-            //smiTest();
-            //morganTest();
-
-            ROMol m3 = RWMol.MolFromSmiles("c1ccccc1");
+            const string SMILES_benzene = "c1ccccc1";
+            ROMol m3 = RWMol.MolFromSmiles(SMILES_benzene);
             uint nAtoms = m3.getNumAtoms(true);
+            Console.WriteLine($"The number of atoms in {SMILES_benzene} is {nAtoms}.");
+        }
 
+        static void BulkMemoryLeakTest()
+        {
             Console.WriteLine("Bulk memory leak test");
             for (uint i = 0; i < 10000; ++i)
             {
-                using (ROMol m4 = RWMol.MolFromSmiles("Clc1cccc(N2CCN(CCC3CCC(CC3)NC(=O)c3cccs3)CC2)c1Cl"))
-                {
-                    if ((i % 1000) == 0)
-                        Console.WriteLine(" Done: " + i);
-                }
+                ROMol m4 = RWMol.MolFromSmiles("Clc1cccc(N2CCN(CCC3CCC(CC3)NC(=O)c3cccs3)CC2)c1Cl");
+                if ((i % 1000) == 0)
+                    Console.WriteLine(" Done: " + i);
+                m4.Dispose();
+                //GC.Collect();
             }
+        }
 
+        static void MakePicture(string smiles, string filename)
+        {
+            int width = 200;
+            int height = 200;
+
+            RWMol mol = null;
+            mol = RWMol.MolFromSmiles(smiles);
+            if (mol == null)
+                mol = RWMol.MolFromSmarts(smiles);
+            if (mol == null)
+                throw new Exception($"Cannot recognize: '{smiles}'");
+
+            MolDraw2DSVG view = new MolDraw2DSVG(width, height);
+            RDKFuncs.prepareMolForDrawing(mol);
+            view.drawMolecule(mol);
+            view.finishDrawing();
+
+            using (var w = new StreamWriter(filename))
+            {
+                w.Write(view.getDrawingText());
+                Console.WriteLine($"{filename} is drawn.");
+            }
+        }
+
+        static void MakePictures()
+        {
+            MakePicture("c1ccccc1", "benzene.svg");
+            MakePicture("Clc1cccc(N2CCN(CCC3CCC(CC3)NC(=O)c3cccs3)CC2)c1Cl", "mol.svg");
+        }
+
+        static void Main(string[] args)
+        {
+            ErrorHandle();
+            Demo();
+            CreateSomeObjects();
+            BulkMemoryLeakTest();
+            MakePictures();
             Console.WriteLine("Goodbye");
         }
     }
